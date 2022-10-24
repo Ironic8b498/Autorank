e19abc728c7493c22b1e2256e4f4414ad8ec1564
Ironic_8b49 <ironic_8b49@yahoo.com>
2022-10-24 17:26:32 -0400

    Attempt to remove files that don't belong on Github

--- src/main/java/me/armar/plugins/utils/pluginlibrary/hooks/afkmanager/AFKManager.java
+++
@@ -1,9 +0,0 @@
-package me.armar.plugins.utils.pluginlibrary.hooks.afkmanager;
-
-import java.util.UUID;
-
-public interface AFKManager {
-    boolean isAFK(UUID var1);
-
-    boolean hasAFKData();
-}
